f2df79ad5866eef8fa88ad7cda6eea69156aaccc
Ishan Raj Singh <srajishan21@gmail.com>
2026-01-09 19:57:16 +0530

    edit and delete

--- contributors/ishanrajsingh/server/src/routes/subscription.routes.js
+++ contributors/ishanrajsingh/server/src/routes/subscription.routes.js
@@ -3,11 +3,15 @@ import requireAuth from '../middleware/requireAuth.js';
 import {
   createSubscription,
   getUserSubscriptions,
+  updateSubscription,
+  deleteSubscription,
 } from '../controllers/subscription.controller.js';
 
 const router = Router();
 
 router.post('/', requireAuth, createSubscription);
 router.get('/', requireAuth, getUserSubscriptions);
+router.put('/:id', requireAuth, updateSubscription);
+router.delete('/:id', requireAuth, deleteSubscription);
 
 export default router;
