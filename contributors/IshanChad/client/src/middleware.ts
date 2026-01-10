b4a1d2f3554acd783e12f33a8f1ed6ca100a032f
Ishan <ishanchadha44@gmail.com>
2026-01-10 23:20:40 +0530

    reverted changes in middleware.ts

--- contributors/IshanChad/client/src/middleware.ts
+++ contributors/IshanChad/client/src/middleware.ts
@@ -1,7 +1,23 @@
-export default function middleware() {
-  // no-op in local dev
-}
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)', // sign-in routes
+  '/sign-up(.*)',
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
-  matcher: [],
+  matcher: [
+    // Skip _next, static files, images, fonts, etc.
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|webp|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
+    // Always run middleware for API & tRPC routes
+    '/(api|trpc)(.*)',
+  ],
 };
