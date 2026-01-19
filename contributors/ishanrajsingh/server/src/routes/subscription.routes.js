f53cdb513f20d15993d9773316ec13ec6b50ea18
Ishan Raj Singh <srajishan21@gmail.com>
2026-01-19 22:43:42 +0530

    Save email subscriptions

--- contributors/ishanrajsingh/server/src/routes/subscription.routes.js
+++ contributors/ishanrajsingh/server/src/routes/subscription.routes.js
@@ -6,10 +6,12 @@ import {
   updateSubscription,
   deleteSubscription,
 } from '../controllers/subscription.controller.js';
+import { importEmailSubscriptions } from '../controllers/subscription.controller.js';
 
 const router = Router();
 
 router.post('/', requireAuth, createSubscription);
+router.post('/email/import', requireAuth, importEmailSubscriptions);
 router.get('/', requireAuth, getUserSubscriptions);
 router.put('/:id', requireAuth, updateSubscription);
 router.delete('/:id', requireAuth, deleteSubscription);
